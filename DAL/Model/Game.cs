61fffc6fe368e1e71212a8f9456bd5f10bc9a259
Jan Strašák <x466533@fi.muni.cz>
2022-10-13 00:13:28 +0200

    Named foreign keys for better understandability

--- DAL/Model/Game.cs
+++ DAL/Model/Game.cs
@@ -1,6 +1,7 @@
 ﻿
 using Discord;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DAL.Model
 {
@@ -14,9 +15,11 @@ namespace DAL.Model
         public ICollection<Room> Rooms { get; set; } = new List<Room>();
         public Role GameRole { get; set; }
         public bool HasActiveRole { get; set; }
+        [ForeignKey("ActiveRoleGameId")]
         public ICollection<Role> ActiveRoles { get; set; } = new List<Role>();
         public Room ActiveCheckRoom { get; set; }
         public Room ModAcceptRoom { get; set; }
+        [ForeignKey("ModAcceptRoleGameId")]
         public ICollection<Role> ModAcceptRoles { get; set; } = new List<Role>();
     }
 }
