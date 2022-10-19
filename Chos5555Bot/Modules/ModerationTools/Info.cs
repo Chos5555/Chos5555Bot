3e0203bdd08ec70d211495628d6ae3f20c2611e2
Jan Strašák <x466533@fi.muni.cz>
2022-10-19 18:11:45 +0200

    Added ping command

---
+++ Chos5555Bot/Modules/ModerationTools/Info.cs
@@ -0,0 +1,19 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chos5555Bot.Modules.ModerationTools
+{
+    public class Info : ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        private async Task Ping()
+        {
+            var time = DateTimeOffset.Now - Context.Message.CreatedAt;
+            await Context.Channel.SendMessageAsync($"🏓 **Pong!**\n**Latency:** {((int)time.TotalMilliseconds)} ms");
+        }
+    }
+}
