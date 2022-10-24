841f5bc3c3d76d904315bfc1efe7187370e56970
Jan Strašák <x466533@fi.muni.cz>
2022-10-25 00:21:46 +0200

    Added new migration

---
+++ DAL/Migrations/20221024182847_RoleModelUpdate.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.Migrations
+{
+    public partial class RoleModelUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Resetable",
+                table: "Roles",
+                newName: "Resettable");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Resettable",
+                table: "Roles",
+                newName: "Resetable");
+        }
+    }
+}
