8933aa8faffb0d7fb39919be52800611f50d9540
Jan Strašák <x466533@fi.muni.cz>
2022-10-25 12:40:32 +0200

    Added property for channel id where to send messages on user leaving the guild

---
+++ DAL/Migrations/20221025103455_GuildUserLeftChannelId.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.Migrations
+{
+    public partial class GuildUserLeftChannelId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "UserLeaveMessageRoomId",
+                table: "Guilds",
+                type: "decimal(20,0)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserLeaveMessageRoomId",
+                table: "Guilds");
+        }
+    }
+}
