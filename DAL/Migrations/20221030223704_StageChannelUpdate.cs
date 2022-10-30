63c33651c0daeb215167b4216e2d6ff086b92b30
Jan Strašák <x466533@fi.muni.cz>
2022-10-31 00:41:52 +0100

    Added new migration for updates in model due to stage channel feature

---
+++ DAL/Migrations/20221030223704_StageChannelUpdate.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.Migrations
+{
+    public partial class StageChannelUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "GuildId",
+                table: "Rooms",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "SpeakerRoleId",
+                table: "Rooms",
+                type: "numeric(20,0)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rooms_GuildId",
+                table: "Rooms",
+                column: "GuildId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Rooms_Guilds_GuildId",
+                table: "Rooms",
+                column: "GuildId",
+                principalTable: "Guilds",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Rooms_Guilds_GuildId",
+                table: "Rooms");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Rooms_GuildId",
+                table: "Rooms");
+
+            migrationBuilder.DropColumn(
+                name: "GuildId",
+                table: "Rooms");
+
+            migrationBuilder.DropColumn(
+                name: "SpeakerRoleId",
+                table: "Rooms");
+        }
+    }
+}
