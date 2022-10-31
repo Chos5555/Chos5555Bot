d44bcc6fd4b828dedb92b38177c32583df86abf6
Jan Strašák <x466533@fi.muni.cz>
2022-10-31 21:01:22 +0100

    Added new migration

---
+++ DAL/Migrations/20221031144710_AddedTextChannelForStage.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.Migrations
+{
+    public partial class AddedTextChannelForStage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "TextForStageId",
+                table: "Rooms",
+                type: "numeric(20,0)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TextForStageId",
+                table: "Rooms");
+        }
+    }
+}
