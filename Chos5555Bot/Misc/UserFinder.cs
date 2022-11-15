7c76e1d9c3447c1d88ccf9acdbf728e2c43d9bfc
Jan Strašák <x466533@fi.muni.cz>
2022-11-15 17:48:58 +0100

    Added UserFinder to search for users by name

---
+++ Chos5555Bot/Misc/UserFinder.cs
@@ -0,0 +1,20 @@
+﻿using Discord;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chos5555Bot.Misc
+{
+    public static class UserFinder
+    {
+        public async static Task<IUser> FindUserByName(string name, IGuild guild)
+        {
+            name = name.ToLower();
+            var users = await guild.GetUsersAsync();
+            return users
+                .Where(u => (u.Username is not null && u.Username.ToLower() == name) ||
+                    (u.Nickname is not null && u.Nickname.ToLower() == name) ||
+                    (u.DisplayName is not null && u.DisplayName.ToLower() == name))
+                .SingleOrDefault();
+        }
+    }
+}
