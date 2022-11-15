3777fcf96d4d45f4b306e36453e7e7fda13ef84c
Jan Strašák <x466533@fi.muni.cz>
2022-11-15 17:48:06 +0100

    Added properties to quest

--- DAL/Model/Quest.cs
+++ DAL/Model/Quest.cs
@@ -8,9 +8,17 @@ namespace DAL.Model
     {
         public int Id { get; set; }
         // Game for which this quest was created
-        public Game Game { get; set; }
-        // ID of message in the quest room
+        public string GameName { get; set; }
+        // Text of the quest
+        public string Text { get; set; }
+        // Id of the user that created this quest
+        public ulong AuthorId { get; set; }
+        // Id of the user that took this quest
+        public ulong TakerId { get; set; }
+        // Id of message in the quest room
         public ulong QuestMessage { get; set; }
+        // Id of the channel the QuestMessage is in
+        public ulong QuestMessageChannelId { get; set; }
         // Id of message in mod quest room
         public ulong ModMessage { get; set; }
     }
